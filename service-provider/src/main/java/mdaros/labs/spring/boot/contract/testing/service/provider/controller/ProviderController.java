d34c90ce0f9d2a75856ebc259b5ebaed6d23c50c
mdaros <massimo.daros@gmail.com>
2020-10-11 22:09:26 +0200

    Added API URI /api/v1/books/{id} and related contract

--- service-provider/src/main/java/mdaros/labs/spring/boot/contract/testing/service/provider/controller/ProviderController.java
+++ service-provider/src/main/java/mdaros/labs/spring/boot/contract/testing/service/provider/controller/ProviderController.java
@@ -4,9 +4,10 @@ import mdaros.labs.spring.boot.contract.testing.service.provider.model.Book;
 import mdaros.labs.spring.boot.contract.testing.service.provider.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ProviderController {
@@ -18,5 +19,11 @@ public class ProviderController {
 	public Flux<Book> getBooks () {
 
 		return service.findBooks ();
+	}
+
+	@GetMapping ( "/api/v1/books/{id}" )
+	public Mono<Book> getBooks ( @PathVariable ( name = "id" ) Long id ) {
+
+		return service.findBook ( id );
 	}
 }
--- service-provider/src/main/java/mdaros/labs/spring/boot/contract/testing/service/provider/service/ProviderService.java
+++ service-provider/src/main/java/mdaros/labs/spring/boot/contract/testing/service/provider/service/ProviderService.java
@@ -3,6 +3,7 @@ package mdaros.labs.spring.boot.contract.testing.service.provider.service;
 import mdaros.labs.spring.boot.contract.testing.service.provider.model.Book;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -16,5 +17,12 @@ public class ProviderService {
 		Book book2 = new Book ( 2L, LocalDateTime.now () );
 
 		return Flux.fromIterable ( Arrays.asList ( book1, book2 ) );
+	}
+
+	public Mono<Book> findBook ( Long id ) {
+
+		Book book1 = new Book ( id, LocalDateTime.now () );
+
+		return Mono.just ( book1 );
 	}
 }
